46e642e9d8b257741c4d9d82d0f2acb549aa18db
ArthurMatMac <macearthur30@gmail.com>
2023-09-06 17:31:25 -0300

    subindo

---
+++ src/test/java/com/example/springnuvemofc/SpringNuvemOfcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springnuvemofc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNuvemOfcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
